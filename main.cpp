b9737361ef386ed6411bf8260949563bfcb49ff1
Dmitry Utkin <dmitryure@gmail.com>
2024-12-21 14:21:29 +0300

    added initial

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/ximgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/tracking/tracker.hpp>
+using namespace std;
+using namespace cv;
+
+void show(string window_name, cv::Mat img)
+{
+    cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
+    cv::moveWindow(window_name, 700, 150);
+    cv::imshow(window_name, img);
+    cv::waitKey(0);
+};
+
+int main()
+{
+    VideoCapture vid = VideoCapture("./videos/kurt_russel_china.avi");
+    Mat frame;
+    for (;;)
+    {
+        vid >> frame;
+        show("Test", frame);
+    }
+    return 0;
+}
