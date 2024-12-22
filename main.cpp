2d2e05cf30d2c522bfa174749d3faf4ad0af178f
Dmitry Utkin <dmitryure@gmail.com>
2024-12-22 21:25:30 +0300

    tracking

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/core.hpp>
+#include <opencv2/line_descriptor.hpp>
 #include <opencv2/ximgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/tracking/tracker.hpp>
+#include <opencv2/tracking.hpp>
+#include <opencv2/ml.hpp>
+#include <opencv2/features2d.hpp>
 #include "utils.h"
 #include "detectors.h"
 #include <dirent.h> // POSIX library for directory traversal
@@ -52,23 +55,70 @@ int show_files()
     return 0;
 }
 
+double euclideanDistance(const cv::Mat &vec1, const cv::Mat &vec2)
+{
+    return cv::norm(vec1, vec2, cv::NORM_L2); // L2 norm (Euclidean distance)
+}
+
+cv::Mat computeHOG(const cv::Mat &img, cv::HOGDescriptor &hog)
+{
+    std::vector<float> descriptors;
+    hog.compute(img, descriptors);
+    return cv::Mat(descriptors).clone();
+}
+
 int main()
 {
     show_files();
+    cv::Mat photo = cv::imread("./images/woman.png", cv::IMREAD_GRAYSCALE);
     VideoCapture vid = VideoCapture(video_path("kurt_russel_china.avi"));
     Mat frame;
+    std::vector<cv::Ptr<cv::Tracker>> trackers;
+    std::vector<Rect> faces;
+
     for (;;)
     {
         vid >> frame;
-
         if (frame.empty())
             break;
-        std::vector<Rect> faces = detectAndDisplay(frame);
+        int frameIndex = static_cast<int>(vid.get(cv::CAP_PROP_POS_FRAMES));
+        if (frameIndex % 10 == 0)
+        {
+            faces = detectAndDisplay(frame);
+            if (faces.size())
+            {
+                trackers.clear();
+                for (size_t face = 0; face < faces.size(); face++)
+                {
+                    auto tracker = cv::TrackerKCF::create();
+                    cv::Rect myROI(faces[face].x, faces[face].y, faces[face].width, faces[face].height);
+                    tracker->init(frame, myROI);
+                    // Resize the ROI to match the HOG descriptor window size
+                    trackers.push_back(tracker);
+                }
+            };
+        };
         if (!faces.size())
             continue;
-        cv::Rect myROI(faces[0].x, faces[0].y, faces[0].width, faces[0].height);
+        for (size_t i = 0; i < faces.size(); i++)
+        {
+            cv::Rect2d myROI(faces[i].x, faces[i].y, faces[i].width, faces[i].height);
+            bool success = trackers[i]->update(frame, myROI);
+
+            // Print feature vector size (optional)
+            if (success)
+            {
+                // Draw the tracked object
+                cv::rectangle(frame, myROI, cv::Scalar(255, 0, 0), 2, 1);
+            }
+            else
+            {
+                std::cerr << "Tracking failure for ROI " << i << std::endl;
+            }
+        }
+
         // cv::Rect myROI(10, 10,100, 100);
-        show("Test", frame(myROI));
+        show("Test", frame);
     }
     return 0;
 }
